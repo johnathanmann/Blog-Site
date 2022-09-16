27b4dffb5c6f9a8c45540bfd68c9d204ce7f99ad
Johnny <johnathan.mnn@gmail.com>
2022-09-15 22:54:05 -0500

    Added models and homepage.handlebars

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    post_contents: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
