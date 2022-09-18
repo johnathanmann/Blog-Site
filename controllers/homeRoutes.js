33b68e568865d1d26e1d96de142ce881753b900b
Johnny <johnathan.mnn@gmail.com>
2022-09-17 21:21:30 -0500

    Content finally displaying

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,12 @@ const router = require('express').Router();
 const { Post } = require('../models/Post');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all users, sorted by name
-    const postData = await Post.findAll().catch((err) => { 
-      res.json(err);
-    });
-    const posts = postData.map((post) => post.get({ plain: true }));
-    // Pass serialized data into Handlebars.js template
-    res.render('main', { posts });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+    try {
+      res.render('posts');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
