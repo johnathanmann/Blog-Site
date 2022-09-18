74953925627d3a66c513fd8d1e0a5b79f60e1455
Johnny <johnathan.mnn@gmail.com>
2022-09-17 21:50:35 -0500

    Heroku test

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post } = require('../models/Post');
 
-router.get('/', async (req, res) => {
+router.get('https://murmuring-escarpment-36262.herokuapp.com//', async (req, res) => {
     try {
       res.render('posts');
     } catch (err) {
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     }
   });
 
-router.get('/login', async (req, res) => {
+router.get('https://murmuring-escarpment-36262.herokuapp.com//login', async (req, res) => {
     try {
       res.render('login');
     } catch (err) {
@@ -17,7 +17,7 @@ router.get('/login', async (req, res) => {
     }
   });
 
-router.get('/sign-up', async (req, res) => {
+router.get('https://murmuring-escarpment-36262.herokuapp.com//sign-up', async (req, res) => {
     try {
       res.render('signup');
     } catch (err) {
